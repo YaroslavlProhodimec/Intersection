279c36168d0de00adddc1f6333051b11d461cdca
TonyMyrato <tonymyrato@TonyMyratos-Air.MyMiFi>
2024-02-01 06:23:32 -0800

    create

---
+++ Intersection.ts
@@ -0,0 +1,15 @@
+/**
+  * Описание задачи: Напишите функцию, которая поверхностно находит пересечения объектов и возвращает объект пересечений.
+  * Ожидаемый результат: ({ a: 1, b: 2 }, { c: 1, b: 2 }) => { b: 2 }
+  * @param {Object<string | number>} firstObj - объект с примитивными значениями
+  * @param {Object<string | number>} secondObj - объект с примитивными значениями
+  * @returns {Object}
+*/
+
+export const intersection = (firstObject, secondObject) => {
+  throw new Error(`Напишите здесь свое решение ${firstObject}, ${secondObject}`);
+};
+
+const data = { a: 1, b: 2 };
+const data2 = { c: 1, b: 2 };
+console.log(intersection(data, data2)); // { b: 2 }
